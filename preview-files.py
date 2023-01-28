ea0b4880786222b3938cc11dfa50b01ef3be3adb
Renardyre <renardyre@gmail.com>
2023-01-28 14:17:21 -0300

    Leaving blank space for image preview

--- preview-files.py
+++ preview-files.py
@@ -6,21 +6,28 @@ import os
 import re
 
 SCRIPT_PATH = os.path.dirname(__file__)
-FEH_IMG_LIST = os.path.join(SCRIPT_PATH, ".img_list")
+IMG_LIST = os.path.join(SCRIPT_PATH, ".img_list")
 
 def main():
-    arg = sys.argv[1:][0].split('\t')
-    index = arg[0]
-    choose = arg[1]
+    arg = sys.argv[1:]
+    choose = arg[0].split('\t')
+    index = choose[0]
+    tree = choose[1]
+    if index == "..": return
 
-    img = os.path.join(SCRIPT_PATH, 'Imgs', f'{index}.jpg')
-    if os.path.exists(img):
-        with open(FEH_IMG_LIST, 'w') as fp:
-            fp.write(img)
+    try: image = arg[1].strip()
+    except: image = ""
 
-    tree = choose.replace('\'', '\"')
+    if image:
+        print('\n'*18)
+        img = os.path.join(SCRIPT_PATH, 'Imgs', f'{index}.jpg')
+        if os.path.exists(img):
+            with open(IMG_LIST, 'w') as fp:
+                fp.write(img)
+
+    print('\n')
+    tree = tree.replace('\'', '\"')
     tree = json.loads(tree)
-    print('\n'*20)
 
     dirs = tree['Dirs']
     files = tree['Files']
@@ -32,7 +39,4 @@ def main():
         print("{}".format('\n'.join([i['Title'] for i in files])))
 
 if __name__ == "__main__":
-    try:
-        main()
-    except:
-        pass
+    main()
